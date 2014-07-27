d7e08d4fd65e15fda305de292080c6d36442918b
arcan1s <esalexeev@gmail.com>
2014-07-27 12:48:42 +0400

    create project tree

---
+++ create_archive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARCHIVE="reportabug"
+SRCDIR="sources"
+FILES="AUTHORS CHANGELOG COPYING README.md"
+IGNORELIST="build *.cppcheck"
+VERSION=$(grep -m1 PROJECT_VERSION_MAJOR sources/CMakeLists.txt | awk '{print $3}' | cut -c 1).\
+$(grep -m1 PROJECT_VERSION_MINOR sources/CMakeLists.txt | awk '{print $3}' | cut -c 1).\
+$(grep -m1 PROJECT_VERSION_PATCH sources/CMakeLists.txt | awk '{print $3}' | cut -c 1)
+# create archive
+[[ -e ${ARCHIVE}-${VERSION}-src.tar.xz ]] && rm -f "${ARCHIVE}-${VERSION}-src.tar.xz"
+[[ -d ${ARCHIVE} ]] && rm -rf "${ARCHIVE}"
+cp -r "${SRCDIR}" "${ARCHIVE}"
+for FILE in ${FILES[*]}; do cp -r "$FILE" "${ARCHIVE}"; done
+for FILE in ${IGNORELIST[*]}; do find "${ARCHIVE}" -name "${FILE}" -exec rm -rf {} \;; done
+tar cJf "${ARCHIVE}-${VERSION}-src.tar.xz" "${ARCHIVE}"
+rm -rf "${ARCHIVE}"
